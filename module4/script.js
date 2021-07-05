eceaa958e0f103a20eca7be5cd3fc59e1f0ba98c
abrah <abrah@xx-ROADKILL-xx.(none)>
2021-07-05 12:47:19 +0530

    week 4 assignment

--- module4/script.js
+++ module4/script.js
@@ -1,11 +1,11 @@
 var names=new Array();
-names[0]="Yaakov";
+names[0]="peter";
 names[1]="John";
-names[2]="Jen";
-names[3]="jason";
+names[2]="Jenny";
+names[3]="jack";
 names[4]="paul";
-names[5]="frank";
-names[6]="larry";
+names[5]="leon";
+names[6]="max";
 names[7]="paula";
 names[8]="laura";
 names[9]="jim";
